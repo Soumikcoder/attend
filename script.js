5f801f522a7354ad7e8fc6e7252e7b95e1888259
Soumik <soumik20020906@gmail.com>
2024-05-15 20:53:15 +0530

    Added Info about no of classes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const textbox = document.querySelector('input');
 const btn = document.getElementById('btn');
 let list = document.querySelector('.list');
 let noOfList=localStorage.length;
+
 // add reset button next
 function createHtml(present,total,subject){
 	let percentage=0;
@@ -19,17 +20,20 @@ function createHtml(present,total,subject){
                 <button id="present">&#x2713;</button>
                 <button id="absent">&#x2717;</button>
                 <button id="edit">&#9998;</button>
-                <button id="del">-</button>`;
+                <button id="del">-</button>
+                <div class="quote">You're right on track.</div>`;
 }
 
 (function getItemsFromLocal(){
 	for(let i=0;i<noOfList;i++){
 		const item = document.createElement('li');
 		item.className='card';
-    	const subinfo =JSON.parse(localStorage.getItem(localStorage.key(i)));
+		const data=localStorage.getItem(localStorage.key(i));
+    	const subinfo =JSON.parse(data);
     	item.innerHTML=createHtml(subinfo.present,subinfo.total,subinfo.subject);
 		item.id=localStorage.key(i);
 		const progressbar = item.querySelector('.progress-bar');
+		const quote = item.querySelector('.quote');
 		percentage=item.querySelector('.progress').textContent;
 		if(percentage >= 75.0){
     		progressbar.style.setProperty('--bgcolor','limegreen');
@@ -37,6 +41,7 @@ function createHtml(present,total,subject){
     	else{
     		progressbar.style.setProperty('--bgcolor','red');
     	}
+    	quote.textContent=getquote(data,percentage);
 		addDeleteListener(item);
 		addPresentListener(item);
 		addEditListener(item);
@@ -88,6 +93,7 @@ function addPresentListener(item) {
     const progressbar = item.querySelector('.progress-bar');
     const presentfield = item.querySelector('.present');
     const totalfield = item.querySelector('.total');
+    const quote=item.querySelector('.quote');
     presentBtn.addEventListener('click', () => {
     	let data=JSON.parse(localStorage.getItem(item.id));
     	data.total++;
@@ -99,6 +105,7 @@ function addPresentListener(item) {
     	if(percentage >= 75.0){
     		progressbar.style.setProperty('--bgcolor','limegreen');
     	}
+    	quote.textContent=getquote(data,percentage);
     	localStorage.setItem(item.id, JSON.stringify(data));
     });
 }
@@ -108,6 +115,7 @@ function addAbsentListener(item) {
     const progress = item.querySelector('.progress');
     const progressbar = item.querySelector('.progress-bar');
     const totalfield = item.querySelector('.total');
+     const quote=item.querySelector('.quote');
     absentBtn.addEventListener('click', () => {
     	let data=JSON.parse(localStorage.getItem(item.id));
     	data.total++;
@@ -117,6 +125,7 @@ function addAbsentListener(item) {
     	if(percentage < 75.0){
     		progressbar.style.setProperty('--bgcolor','red');
     	}
+    	quote.textContent=getquote(data,percentage);
     	localStorage.setItem(item.id, JSON.stringify(data));
     });
 }
@@ -134,8 +143,8 @@ function addEditListener(item){
 	const editbtn=item.querySelector('#edit');
 	const progress = item.querySelector('.progress');
 	const progressbar = item.querySelector('.progress-bar');
+	const quote=item.querySelector('.quote');
 	editbtn.addEventListener('click',()=>{
-		console.log(presentfield);
 		if(presentfield[0].readOnly){
 			presentfield.forEach(field=>{
 				field.readOnly=false;
@@ -150,8 +159,7 @@ function addEditListener(item){
     	data.total=parseInt(presentfield[1].value);
     	data.present=parseInt(presentfield[0].value);
     	if(data.total>0){
-
-    	percentage=data.present/data.total*100;
+    		percentage=data.present/data.total*100;
     	}
     	else{
     		percentage=100;
@@ -164,6 +172,7 @@ function addEditListener(item){
     	else{
     		progressbar.style.setProperty('--bgcolor','red');
     	}
+    	quote.textContent=getquote(data,percentage);
     	editbtn.style.background="orange";
     	localStorage.setItem(item.id, JSON.stringify(data));
 		}
@@ -177,3 +186,28 @@ const helpbtn=document.getElementById('help');
 helpbtn.addEventListener('click',()=>{
 	help.setAttribute('style','transform: scale(1.0) ;transition: 0.2s;');
 })
+
+function getquote(data,datapercentage){
+	const percentage=75.0
+	if(datapercentage >= percentage){
+    		const noOfLeaves=Math.floor((100*data.present-percentage*data.total)/percentage);
+    		if(noOfLeaves == 1){
+    			return `You may Leave next class.`;
+    		}
+    		else if(noOfLeaves > 0){
+    			return `You may Leave next ${noOfLeaves} classes.`;
+    		}
+    		else{
+    			return`You're right on track.`;
+    		}
+    	}
+    	else{
+    		const noOfAttend=Math.floor((percentage*data.total-100*data.present)/(100-percentage));
+    		if(noOfAttend <= 1){
+    			return `You have to attend next class.`;
+    		}
+    		else if(noOfAttend > 0){
+    			return `You have to attend next ${noOfAttend} classes.`;
+    		}
+    	}
+}
