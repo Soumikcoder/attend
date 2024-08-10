173f95ee68e066d20755c5ce745bfbeed05dbce5
Soumik <soumik20020906@gmail.com>
2024-08-10 20:19:30 +0530

    everything added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import {CardProvider} from './context'
+import InputField from './components/InputField.jsx'
+import CardList from './components/CardList.jsx'
+import Help from './components/Help.jsx'
+import Slider from './components/Slider.jsx'
 function App() {
-  const [count, setCount] = useState(0)
+  const [req_percentage,setReqPercentage]=useState(75)
+  const [cards,setCards]=useState([])
+
+  const addCard=(subname)=>{
+    if(subname==="") return
+    const card={
+      id:btoa(subname),
+      name:subname,
+      present:0,
+      total:0
+    }
+    for(let i=0;i<cards.length;i++)
+      if(cards[i].id === card.id)
+        return false
+    setCards((prev)=>[...prev,card])
+    return true
+  }
+  const editCard=(id,card)=>{
+    setCards((prev)=>(prev.map((prev_card)=> prev_card.id === id ? card : prev_card
+    )))
+  }
+
+  const deleteCard=(id)=>{
+    setCards((prev)=>(prev.filter((card)=>card.id!==id)))
+  }
+  useEffect(()=>{
+    const cards_from_storage=JSON.parse(localStorage.getItem("Attendance"))
+
+    if(cards_from_storage && cards_from_storage.length > 0){
+      setCards(cards_from_storage)
+    }
+    const storage_percentage=JSON.parse(localStorage.getItem("percentage"))
+    if(storage_percentage) {
+      changePercentage(storage_percentage)
+    }
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("Attendance",JSON.stringify(cards))
+  },[cards])
+
+  const changePercentage=(percentage)=>{
+    setReqPercentage(percentage)
+    localStorage.setItem("percentage",percentage)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+    <CardProvider value={{req_percentage,changePercentage,cards,addCard,editCard,deleteCard}}>
+    <div className='subject'>
+      <h1>Attendance Manager</h1>
+      <InputField />
+      <div className='list'>
+      {
+        cards.map((card)=>{
+          return (<CardList key={card.id} cardcomp={card} />)
+        })
+      }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Help/>
+      <Slider/>
+    </CardProvider>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// import { VitePWA } from 'vite-plugin-pwa';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    basicSsl(),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   manifest: {
+    //     name: 'Attendance Manager',
+    //     short_name: 'Attendance',
+    //     description: 'Maintain your attendance',
+    //     theme_color: '#ffffff',
+    //     icons: [
+    //       {
+    //         src: '/vite.svg',
+    //         sizes: '192x192',
+    //         type: 'image/svg+xml'
+    //       }
+    //       // ,
+    //       // {
+    //       //   src: '/icon-512x512.png',
+    //       //   sizes: '512x512',
+    //       //   type: 'image/png'
+    //       // }
+    //     ]
+    //   }
+    // })
+  ]
+});
