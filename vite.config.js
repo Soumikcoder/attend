88a4a0cd0bf016a804c41a5528b7e4349d8da9f3
Soumik <soumik20020906@gmail.com>
2024-08-11 11:38:12 +0530

    added worker

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
   plugins: [
     react(),
     basicSsl(),
-    // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   manifest: {
-    //     name: 'Attendance Manager',
-    //     short_name: 'Attendance',
-    //     description: 'Maintain your attendance',
-    //     theme_color: '#ffffff',
-    //     icons: [
-    //       {
-    //         src: '/vite.svg',
-    //         sizes: '192x192',
-    //         type: 'image/svg+xml'
-    //       }
-    //       // ,
-    //       // {
-    //       //   src: '/icon-512x512.png',
-    //       //   sizes: '512x512',
-    //       //   type: 'image/png'
-    //       // }
-    //     ]
-    //   }
-    // })
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Attendance Manager',
+        short_name: 'Attendance',
+        description: 'Maintain your attendance',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          }
+          // ,
+          // {
+          //   src: '/icon-512x512.png',
+          //   sizes: '512x512',
+          //   type: 'image/png'
+          // }
+        ]
+      }
+    })
   ],
    base: '/attend/'
 });
